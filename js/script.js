31338a1c721a83035d5aa97be30783148104ef90
mirkone1994 <mirkochiappori@gmail.com>
2021-06-30 15:05:09 +0200

    Added message

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+var message = "Questa è la tua password generata: "
 var name = prompt("Quale è il tuo nome?");
 var surname = prompt("Quale è il tuo cognome?");
 var color = prompt("Quale è il tuo colore preferito?");
 var randomNumber = 21
 var passwordPlaceholder = document.getElementById("password");
-passwordPlaceholder.innerHTML = name + surname + color + randomNumber;
+passwordPlaceholder.innerHTML = message + name + surname + color + randomNumber;
 console.log(name, surname, color, randomNumber, passwordPlaceholder)
